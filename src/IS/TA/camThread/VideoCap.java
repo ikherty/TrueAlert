556e078a7988b8c7665ed5d28b1f9d43370ba0b8
Valentina <ikherty@gmail.com>
2020-06-13 21:54:06 +0300

    add getters for buffered image

--- src/IS/TA/camThread/VideoCap.java
+++ src/IS/TA/camThread/VideoCap.java
@@ -5,7 +5,10 @@
  */
 package IS.TA.camThread;
 
+import static IS.TA.recognize.ObjectDetect.MatToBufferedImage;
 import static IS.TA.recognize.ObjectDetect.frontalface_alt;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import org.opencv.core.Core;
@@ -17,6 +20,7 @@ import org.opencv.videoio.VideoCapture;
  *
  * @author Petrenko Valentina
  */
+
 public class VideoCap {
     //public static void main(String args[]) {//для теста этого метода
     public static void ShowVideo() {//статический метод для вызова из главного фрейма
@@ -31,16 +35,44 @@ public class VideoCap {
             while (true) {
                 if (camera.read(frame)) {
                     Mat newframe = frontalface_alt(frame);//выделение лиц в прямоугольники
-//                      вывод информации о потоке
-//    	    		System.out.println("Frame Obtained");
-//    	    		System.out.println("Captured Frame Width " + 
-//    	    		frame.width() + " Height " + frame.height());
                     HighGui.imshow("True Alert", newframe);//вывод в новом окне обработанного фрейма
                     HighGui.waitKey(2);//2-delay
                 }
             }
         }
         camera.release();
+    }
+    
+     public static Image getFrameFromCam() {//возвращает фрейм
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        VideoCapture camera = new VideoCapture(0);
+        BufferedImage bufferedImage = null;
+        if (!camera.isOpened()) {
+            JOptionPane.showMessageDialog(new JFrame(), "Камера не подключена!", "Предупреждение", JOptionPane.WARNING_MESSAGE);
+        } else {
+            Mat frame = new Mat();
+            if (camera.read(frame)) {
+                bufferedImage = MatToBufferedImage(frame);
+            }
+        }
+        camera.release();
+        return bufferedImage;
+    }
+     
+     public static Image getRectFrameFromCam() {//возвращает обработанный фрейм
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        VideoCapture camera = new VideoCapture(0);
+        BufferedImage bufferedImage = null;
+        if (!camera.isOpened()) {
+            JOptionPane.showMessageDialog(new JFrame(), "Камера не подключена!", "Предупреждение", JOptionPane.WARNING_MESSAGE);
+        } else {
+            Mat frame = new Mat();
+            if (camera.read(frame)) {
+                bufferedImage = MatToBufferedImage(frontalface_alt(frame));
+            }
+        }
+        camera.release();
+        return bufferedImage;
     }
 }
 // Вероятно, этот код пригодится для встраивания в главное окно
